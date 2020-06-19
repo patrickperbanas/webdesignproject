54b310696b248ad21abb9a65468dc9dd677ba02f
nabasapatrick <if314023@gmail.com>
2020-06-19 14:49:35 +0700

    complete migrate

---
+++ project-root/app/Database/Migrations/2020-06-15-122734_user.php
@@ -0,0 +1,43 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class User extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'user_id'          => [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'unsigned'       => TRUE,
+				'auto_increment' => TRUE
+			],
+			'username'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'nik' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'password' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'role' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+		]);
+		$this->forge->addKey('user_id', TRUE);
+		$this->forge->createTable('user');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		//
+	}
+}
---
+++ project-root/app/Database/Migrations/2020-06-19-074628_izin.php
@@ -0,0 +1,53 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Izin extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id_izin'	=>[
+			'type'	=> 'INT',
+			'constraint'	=> 11,
+			'unsigned'	=> TRUE,
+			'auto_increment' => TRUE
+			],
+			'nik'	=>[
+			'type'	=> 'INT',
+			'constraint'	=> 50,
+			],
+			'izin_type'	=>[
+			'type'	=> 'INT',
+			'constraint'	=> 5,
+			],
+			'start_date'	=>[
+			'type'	=> 'DATE',
+			],
+			'end_date'	=>[
+			'type'	=> 'DATE',
+			],
+			'estimasi_biaya'	=>[
+			'type'	=> 'DECIMAL',
+			'constraint'	=> 18,2,
+			],
+			'status'	=>[
+			'type'	=> 'VARCHAR',
+			'constraint'	=> 20,
+			],
+			'history_perjalanan'	=>[
+			'type'	=> 'VARCHAR',
+			'constraint'	=> 100,
+			],
+		]);
+		$this->forge->addKey('id_izin', TRUE);
+		$this->forge->createTable('izin');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		//
+	}
+}
