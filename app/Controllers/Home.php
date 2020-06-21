4261557ccea9918e276e109b94bc6d4ad4628eda
rynalldo <aldo@premiro.com>
2020-06-21 18:58:06 +0700

    new dashboard with adminlte

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,9 +4,51 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$data= [
+			'title' => 'Judul Home',
+			'isi' => 'v_home',
+		];
+		echo view('layout/v_wrapper', $data);
 	}
 
-	//--------------------------------------------------------------------
+	public function cuti()
+	{
+		$data= [
+			'title' => 'Judul Menu Cuti',
+			'isi' => 'v_cuti',
+		];
+		echo view('layout/v_wrapper', $data);
+	}
+
+
+	public function laporan()
+	{
+		$data= [
+			'title' => 'Judul Menu Laporan',
+			'isi' => 'v_laporan',
+		];
+		echo view('layout/v_wrapper', $data);
+	}
+
+
+	public function perjalananBisnis()
+	{
+		$data= [
+			'title' => 'Judul Menu Perjalanan Bisnis',
+			'isi' => 'v_perjalananbisnis',
+		];
+		echo view('layout/v_wrapper', $data);
+	}
+
+	public function profil()
+	{
+		$data= [
+			'title' => 'Judul Menu Profil',
+			'isi' => 'v_profil',
+		];
+		echo view('layout/v_wrapper', $data);
+	}
+
+
 
 }
